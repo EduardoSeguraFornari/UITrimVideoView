e334daccd2272897655957bef636f223ab1fba86
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-08-12 20:52:07 -0300

    first commit

---
+++ TrimVideoView/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TrimVideoView
+//
+//  Created by Eduardo Fornari on 12/08/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ TrimVideoView/ViewControllers/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  TrimVideoView
+//
+//  Created by Eduardo Fornari on 12/08/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
